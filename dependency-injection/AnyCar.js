5ab285803e9064bf7fd387cec5ed407d836545de
slached <omerbsahin@gmail.com>
2024-12-13 01:17:33 +0300

    dep injection without awilix

---
+++ dependency-injection/AnyCar.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.FuelType = void 0;
+var FuelType;
+(function (FuelType) {
+    FuelType["Diesel"] = "Diesel";
+    FuelType["Gasoline"] = "Gasoline";
+})(FuelType || (exports.FuelType = FuelType = {}));
