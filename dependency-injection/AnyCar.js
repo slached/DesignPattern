9b80f3b8215dc0aebd9a5932a6dbed65e51b0b20
Ömer Bircan Sahin <omerbsahin@gmail.com>
2024-12-15 20:25:07 +0300

    Delete dependency-injection/AnyCar.js

--- dependency-injection/AnyCar.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FuelType = void 0;
-var FuelType;
-(function (FuelType) {
-    FuelType["Diesel"] = "Diesel";
-    FuelType["Gasoline"] = "Gasoline";
-})(FuelType || (exports.FuelType = FuelType = {}));
