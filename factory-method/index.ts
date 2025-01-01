fc3c48b980d0a454ec55ad62483488a545930976
slached <omerbsahin@gmail.com>
2025-01-01 23:09:19 +0300

    factory method

--- factory-method/index.ts
+++ factory-method/index.ts
@@ -1,43 +1,43 @@
-interface IVehicle {
-  getName(): string;
+interface ICar {
+  getBrand(): string;
 }
 
-class Car implements IVehicle {
-  getName(): string {
+class Car implements ICar {
+  getBrand(): string {
     return "Car";
   }
 }
 
-class Bike implements IVehicle {
-  getName(): string {
+class Bike implements ICar {
+  getBrand(): string {
     return "Bike";
   }
 }
 
-class Truck implements IVehicle {
-  getName(): string {
+class Truck implements ICar {
+  getBrand(): string {
     return "Truck";
   }
 }
 
 abstract class VehicleFactory {
-  abstract createVehicle(): IVehicle;
+  abstract createVehicle(): ICar;
 }
 
 class CarFactory extends VehicleFactory {
-  createVehicle(): IVehicle {
+  createVehicle(): ICar {
     return new Car();
   }
 }
 
 class BikeFactory extends VehicleFactory {
-  createVehicle(): IVehicle {
+  createVehicle(): ICar {
     return new Bike();
   }
 }
 
 class TruckFactory extends VehicleFactory {
-  createVehicle(): IVehicle {
+  createVehicle(): ICar {
     return new Truck();
   }
 }
@@ -51,6 +51,6 @@ const car = carFactory.createVehicle();
 const bike = bikeFactory.createVehicle();
 const truck = truckFactory.createVehicle();
 
-console.log(car.getName()); // Output: Car
-console.log(bike.getName()); // Output: Bike
-console.log(truck.getName()); // Output: Truck
+console.log(car.getBrand()); // Output: Car
+console.log(bike.getBrand()); // Output: Bike
+console.log(truck.getBrand()); // Output: Truck
